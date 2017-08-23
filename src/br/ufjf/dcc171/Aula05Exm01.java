09e92cd13b734c8d6cff8209ccb1f4be982093b7
ice <ice@lab06m27.nrc.ice.ufjf.br>
2017-08-22 21:57:00 -0300

    Finalizando 2

--- src/br/ufjf/dcc171/Aula05Exm01.java
+++ src/br/ufjf/dcc171/Aula05Exm01.java
@@ -6,7 +6,7 @@ public class Aula05Exm01 {
 
     public static void main(String[] args) {
         JanelaCheckBox janela = new JanelaCheckBox();
-        janela.setSize(300, 100);
+        janela.setSize(300, 200);
         janela.setLocationRelativeTo(null);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setVisible(true);
